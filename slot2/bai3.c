fd73be548b1f66aae417fca31d08d7d2673b2379
unknown <mdat1710@gmail.com>
2026-01-09 11:35:39 +0700

    bai3

---
+++ slot2/bai3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    // Khai bao 2 bien so thuc a, b va khoi tao gia tri
+    float a = 3.5;
+    float b = 2.5;
+
+    // Khai bao bien kq la ket qua cua a + b
+    float kq = a + b;
+
+    // In a
+    printf("Gia tri cua a = %.2f\n", a);
+
+    // In b
+    printf("Gia tri cua b = %.2f\n", b);
+
+    // In ket qua: kq
+    printf("Tong a + b = %.2f", kq);
+
+    return 0;
+}
