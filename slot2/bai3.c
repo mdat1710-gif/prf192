f8a79749084c4d86e0c5737abfd39ae7ceccc2c2
unknown <mdat1710@gmail.com>
2026-01-09 11:43:45 +0700

    bai 3

--- slot2/bai3.c
+++ slot2/bai3.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    // Khai bao 2 bien so thuc a, b va khoi tao gia tri
-    float a = 3.5;
+    // Khai bao 1 bien so thuc a, b va khoi tao gia tri
+    float a = 3;
     float b = 2.5;
 
     // Khai bao bien kq la ket qua cua a + b
     float kq = a + b;
 
     // In a
-    printf("Gia tri cua a = %.2f\n", a);
+    printf("Gia tri cua a = %.0f\n", a);
 
     // In b
     printf("Gia tri cua b = %.2f\n", b);
