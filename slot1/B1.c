2ca9f4ef56bc318b703f7a6665c96a08c0610c4f
unknown <mdat1710@gmail.com>
2026-01-09 09:42:38 +0700

    add slot1 folder

---
+++ slot1/B1.c
@@ -0,0 +1,12 @@
+#include<limits.h>
+int main(){
+	char hoten[]= "dat";
+	int tuoi =  19;
+	char uudiem[]= "hansome";
+	char nhuocdiem[]= "lying";
+	//in ra ket qua 
+	printf("Ho ten: %s \n",hoten);
+	printf("Tuoi: %d \n",tuoi);
+	printf("Uu diem: %s \n",uudiem);
+	printf("Nhuoc diem: %s",nhuocdiem);
+}
