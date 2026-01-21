0e5679e12d9fc6836ef8edf1bded3e2b00b13202
unknown <mdat1710@gmail.com>
2026-01-21 08:40:49 +0700

    slot5

---
+++ slot5/ex1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	int x;
+	printf ("Nhap x: ");
+	scanf ("%d",&x);
+	int preX = x;
+	if (x<0)
+	{
+		x=-x;
+	}
+	printf("|%d| = %d",preX,x);
+	printf ("\ncan bac 2 la: %2f",sqrt(x));
+	return 0;
+}
