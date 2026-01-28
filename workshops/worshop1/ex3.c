fc6d5e485d4968da3c2dd11fab357146e8422a5b
unknown <mdat1710@gmail.com>
2026-01-28 07:30:02 +0700

    add folder slot5 to workshop

---
+++ workshops/worshop1/ex3.c
@@ -0,0 +1,23 @@
+//dung if-else 
+#include<stdio.h>
+int main()
+{
+	float a,b,c;
+	printf ("Nhap a,b,c:");
+	scanf ("%f,%f,%f",&a,&b,&c);
+	float max;
+	if (a>b && a>c)
+		max = a;
+	else {
+		if (b>a && b>c)
+		{
+			max = b; 
+		}
+		else 
+		{
+			max = c;
+		}
+			
+	}
+		printf ("So lon nhat la: %.2f",max);
+}
